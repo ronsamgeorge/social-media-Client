78268082ad88497b5906ea37f2226893f78ad04c
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-16 15:45:55 +0530

    Add left and right Side bars

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ function App() {
         <NavBar />
         <div style={{ display: "flex" }}>
           <LeftBar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+
           <RightBar />
         </div>
       </div>
