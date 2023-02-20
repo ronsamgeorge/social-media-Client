02d027b8b1c72ff17e01dfd5963a0afb8acf3173
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-20 23:14:04 +0530

    Add Profile Page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,7 +1,76 @@
+import {
+  FacebookTwoTone,
+  LinkedIn,
+  Instagram,
+  Pinterest,
+  Twitter,
+  Place,
+  Language,
+  EmailOutlined,
+  MoreVert,
+} from "@mui/icons-material";
+import Posts from "../../components/posts/Posts";
+
 import "./profile.scss";
 
 const Profile = () => {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="cover"
+        />
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profile"
+        />
+      </div>
+      <div className="profileContainer">
+        <div className="uInfo">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoTone fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Instagram fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Twitter fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <LinkedIn fontSize="large" />
+            </a>
+            <a href="http://facebook.com">
+              <Pinterest fontSize="large" />
+            </a>
+          </div>
+          <div className="center">
+            <span>Jane Doe</span>
+            <div className="info">
+              <div className="item">
+                <Place />
+                <span>USA</span>
+              </div>
+
+              <div className="item">
+                <Language />
+                <span>example.dev</span>
+              </div>
+            </div>
+            <button>Follow</button>
+          </div>
+          <div className="right">
+            <EmailOutlined />
+            <MoreVert />
+          </div>
+        </div>
+        <Posts />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
