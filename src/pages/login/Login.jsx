d3caa2d3559f06d4d4bb042b50c064c015433953
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-25 12:14:22 +0530

    Contect Login Pages with API for credential check

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,13 +1,32 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/authContext";
 import "./login.scss";
 
 const Login = () => {
   const { login } = useContext(AuthContext);
 
-  const handleLogin = () => {
-    login();
+  const navigate = useNavigate();
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const handleChange = (e) => {
+    setInputs((prevs) => ({ ...prevs, [e.target.name]: e.target.value }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(inputs);
+      navigate("/");
+    } catch (error) {
+      setErrorMsg(error.response.data);
+    }
   };
 
   return (
@@ -29,8 +48,19 @@ const Login = () => {
         <div className="right">
           <h1>Login</h1>
           <form>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+            />
+            {errorMsg && errorMsg}
             <button onClick={handleLogin}>LOGIN</button>
           </form>
         </div>
