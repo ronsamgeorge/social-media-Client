e783f5496068cef2fe5b307d41a339b8132fe99e
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-28 00:04:58 +0530

    Add comments to Posts

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,35 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
 import "./comments.scss";
 
 import { useQuery } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 import moment from "moment";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
+  const queryClient = new useQueryClient();
+  const [desc, setDesc] = new useState("");
 
-  const { isLoading, error, data } = useQuery(["commentss"], () =>
+  const { isLoading, error, data } = useQuery(["comments"], () =>
     makeRequest.get("/comments?postId=" + postId).then((res) => {
       return res.data;
     })
   );
 
+  const mutation = useMutation(
+    (newComment) => {
+      return makeRequest.post("/comments", newComment);
+    },
+    {
+      onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries(["comments"]);
+      },
+    }
+  );
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    mutation.mutate({ desc, postId });
+    setDesc("");
+  };
+
+  console.log(desc);
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="write a comment" />
-        <button>Comment</button>
+        <input
+          type="text"
+          placeholder="write a comment"
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <button onClick={handleClick}>Comment</button>
       </div>
       {isLoading
         ? "Loading "
         : data.map((comment) => (
             <div className="comment">
-              <img src={comment.profilePicture} alt="" />
+              <img src={comment.profilePic} alt="" />
               <div className="info">
                 <span>{comment.name}</span>
-                <p>{comment.desc}</p>
+                <p>{comment.des}</p>
               </div>
               <span className="date">
                 {moment(comment.createdAt).fromNow()}
